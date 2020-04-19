07bc7344376f30dbec49de18154cda80cf19d48d
Farshad Nematdoust <farshad@nematdoust.com>
2020-04-19 22:06:53 +0200

    Add encryption/decryption methods

---
+++ key-manager/messages.go
@@ -0,0 +1,27 @@
+package key_manager
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha512"
+)
+
+func EncryptMessage(msg []byte, k *rsa.PublicKey) ([]byte, error) {
+	hash := sha512.New()
+	ct, err := rsa.EncryptOAEP(hash, rand.Reader, k, msg, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return ct, nil
+}
+
+func DecryptMessage(msg []byte, k *rsa.PrivateKey) ([]byte, error) {
+	hash := sha512.New()
+	pt, err := rsa.DecryptOAEP(hash, rand.Reader, k, msg, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return pt, err
+}
