9407cc827c9f22b738e4070880f0a2f8bdb80e3c
Farshad Nematdoust <farshad@nematdoust.com>
2020-04-19 22:04:44 +0200

    Add rsa key generator

---
+++ key-manager/key-generator.go
@@ -0,0 +1,33 @@
+package key_manager
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+)
+
+func generatePrivateKey() (*rsa.PrivateKey, error) {
+	key, err := rsa.GenerateKey(rand.Reader, KeySize)
+	if err != nil {
+		return nil, err
+	}
+
+	err = key.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	return key, nil
+}
+
+func convertPrivateKeyToByte(k *rsa.PrivateKey) []byte {
+	key := x509.MarshalPKCS1PrivateKey(k)
+
+	keyBlock := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: key,
+	}
+	b := pem.EncodeToMemory(keyBlock)
+	return b
+}
