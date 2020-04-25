c30e8246fce72d2444d39991c130ba79adb3ce12
Farshad Nematdoust <farshad@nematdoust.com>
2020-04-25 14:06:24 +0200

    Add a method to helper to retrieve abs path of keys directory

--- key-manager/helpers.go
+++ key-manager/helpers.go
@@ -1,6 +1,7 @@
 package keyManager
 
 import (
+	"path/filepath"
 	"strconv"
 	"time"
 )
@@ -9,4 +10,13 @@ func GetKeyId() string {
 	t := time.Now()
 	// todo: this seems stupid, find a better way...
 	return strconv.Itoa(t.Year()) + strconv.Itoa(int(t.Month()))
+}
+
+func getKeyPath() (string, error) {
+	p, err := filepath.Abs(StorageDirectory)
+	if err != nil {
+		return "", err
+	}
+
+	return p, nil
 }
