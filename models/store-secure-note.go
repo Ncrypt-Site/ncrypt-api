eb357f1b344aeae1d847e78827237dd114f6d7ca
Farshad Nematdoust <farshad@nematdoust.com>
2020-05-05 18:26:45 +0200

    Update Validation to allow zero value, hence the field is not required anymore.

--- models/store-secure-note.go
+++ models/store-secure-note.go
@@ -2,7 +2,7 @@ package models
 
 type SecureMessageRequest struct {
 	Message              string `json:"message" validate:"required"`
-	SelfDestruct         int    `json:"self_destruct" validate:"required,oneof=0 1 3 6 12 24 48 72 168 720"`
+	SelfDestruct         int    `json:"self_destruct" validate:"oneof=0 1 3 6 12 24 48 72 168 720"`
 	Password             string `json:"password" validate:"omitempty,min=8,max=24"`
 	DestructAfterOpening bool   `json:"destruct_after_opening" validate:"omitempty"`
 }
