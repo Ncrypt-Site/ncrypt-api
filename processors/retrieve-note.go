86acb74cdaed1c5c6637538a2e70c710231bac53
Farshad Nematdoust <farshad@nematdoust.com>
2020-05-11 20:51:09 +0200

    Update retrieve process to ignore keys and password

--- processors/retrieve-note.go
+++ processors/retrieve-note.go
@@ -5,28 +5,15 @@ import (
 	"errors"
 	"github.com/go-redis/redis/v7"
 	"github.com/google/uuid"
-	"ncrypt-api/cypher"
-	keyManager "ncrypt-api/key-manager"
 	"ncrypt-api/models"
 )
 
-func RetrieveSecureNote(client *redis.Client, currentKey models.Key, payload models.RetrieveNoteRequest) ([]byte, error) {
+func RetrieveSecureNote(client *redis.Client, payload models.RetrieveNoteRequest) ([]byte, error) {
 	if !checkIfNoteExists(client, payload.Id) {
 		return nil, errors.New("note does not exist")
 	}
 
 	note, err := retrieveNote(client, payload.Id)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(note.Password) != 0 {
-		if !cypher.ValidatePassword([]byte(payload.Password), []byte(note.Password)) {
-			return nil, errors.New("invalid password provided")
-		}
-	}
-
-	noteDecoded, err := decryptNote(note.Message, note.KeyId, currentKey)
 	if err != nil {
 		return nil, err
 	}
@@ -39,7 +26,7 @@ func RetrieveSecureNote(client *redis.Client, currentKey models.Key, payload mod
 		}
 	}
 
-	return noteDecoded, nil
+	return note.Message, nil
 }
 
 func checkIfNoteExists(client *redis.Client, id uuid.UUID) bool {
@@ -64,29 +51,6 @@ func retrieveNote(client *redis.Client, id uuid.UUID) (models.SecureMessage, err
 	}
 
 	return secureMessage, nil
-}
-
-// todo: this probably can be improved, think about it.
-func decryptNote(note []byte, keyId string, k models.Key) ([]byte, error) {
-	var message []byte
-	var err error
-
-	if keyId != keyManager.GetKeyId() {
-		privateKey, err := keyManager.LoadKey(keyId)
-		if err != nil {
-			return nil, err
-		}
-
-		message, err = cypher.DecryptMessage(note, privateKey)
-	} else {
-		message, err = cypher.DecryptMessage(note, k.PrivateKey)
-	}
-
-	if err != nil {
-		return nil, err
-	}
-
-	return message, nil
 }
 
 func deleteNote(client *redis.Client, id uuid.UUID) error {
