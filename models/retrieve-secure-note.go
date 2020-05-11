e9e6af86a50c7f95ed47d101fe1860c778a81f26
Farshad Nematdoust <farshad@nematdoust.com>
2020-05-11 20:49:53 +0200

    Remove password from store and retrieve models as it's delegated to the client

--- models/retrieve-secure-note.go
+++ models/retrieve-secure-note.go
@@ -3,8 +3,7 @@ package models
 import "github.com/google/uuid"
 
 type RetrieveNoteRequest struct {
-	Id       uuid.UUID `json:"id" validate:"required"`
-	Password string    `json:"password" validate:"omitempty,min=8,max=24"`
+	Id uuid.UUID `json:"id" validate:"required"`
 }
 
 type RetrieveNoteResponse struct {
--- models/store-secure-note.go
+++ models/store-secure-note.go
@@ -3,7 +3,6 @@ package models
 type SecureMessageRequest struct {
 	Message              string `json:"message" validate:"required"`
 	SelfDestruct         int    `json:"self_destruct" validate:"oneof=0 1 3 6 12 24 48 72 168 720"`
-	Password             string `json:"password" validate:"omitempty,min=8,max=24"`
 	DestructAfterOpening bool   `json:"destruct_after_opening" validate:"omitempty"`
 }
 
@@ -14,7 +13,5 @@ type SecureMessageResponse struct {
 
 type SecureMessage struct {
 	Message              []byte
-	KeyId                string
-	Password             string
 	DestructAfterOpening bool
 }
