740c50940d18d729c82a2397c6cb1dac59cc6ddf
Farshad Nematdoust <farshad@nematdoust.com>
2020-05-15 20:16:37 +0200

    Update retrieve note processor to use storage interface, method signature is also updated.

--- processors/retrieve-note.go
+++ processors/retrieve-note.go
@@ -1,26 +1,23 @@
 package processors
 
 import (
-	"encoding/json"
 	"errors"
-	"github.com/go-redis/redis/v7"
-	"github.com/google/uuid"
 	"ncrypt-api/models"
 )
 
-func RetrieveSecureNote(client *redis.Client, payload models.RetrieveNoteRequest) ([]byte, error) {
-	if !checkIfNoteExists(client, payload.Id) {
+func RetrieveSecureNote(storage models.StorageInterface, payload models.RetrieveNoteRequest) ([]byte, error) {
+	if !storage.Exists(payload.Id) {
 		return nil, errors.New("note does not exist")
 	}
 
-	note, err := retrieveNote(client, payload.Id)
+	note, err := storage.Retrieve(payload.Id)
 	if err != nil {
 		return nil, err
 	}
 
 	// todo: need improvement and better handling
 	if note.DestructAfterOpening {
-		err := deleteNote(client, payload.Id)
+		err := storage.Delete(payload.Id)
 		if err != nil {
 			return nil, err
 		}
