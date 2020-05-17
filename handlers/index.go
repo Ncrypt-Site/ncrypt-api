89d23efef4734fdacc5ac8a9627cf53ce5761d9d
Farshad Nematdoust <farshad@nematdoust.com>
2020-05-17 13:38:43 +0200

    Update Index to use http constant for response code.

--- handlers/index.go
+++ handlers/index.go
@@ -2,8 +2,9 @@ package handlers
 
 import (
 	"github.com/labstack/echo"
+	"net/http"
 )
 
 func (di *DI) Index(c echo.Context) error {
-	return c.Redirect(301, "/api/v1")
+	return c.Redirect(http.StatusMovedPermanently, "/api/v1")
 }
