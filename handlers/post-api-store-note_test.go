a030e2ef7e0eb44f3a952e67f8494bfc40f0db7a
Farshad Nematdoust <farshad@nematdoust.com>
2020-05-17 15:04:16 +0200

    Add test for post endpoint for a case that storage fails

--- handlers/post-api-store-note_test.go
+++ handlers/post-api-store-note_test.go
@@ -2,19 +2,24 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
 	"github.com/go-playground/validator/v10"
+	"github.com/google/uuid"
 	"github.com/labstack/echo"
 	"ncrypt-api/config"
 	"ncrypt-api/models"
 	"ncrypt-api/storage"
+	redisStorage "ncrypt-api/storage/redis-storage"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
 )
 
 var (
 	postInvalidPayload = `{"message":"this is my branch","self_destruct": 2,"destruct_after_opening": true}`
+	postValidPayload   = `{"message":"this is my branch","self_destruct": 0,"destruct_after_opening": true}`
 )
 
 type customValidator struct {
@@ -23,6 +28,18 @@ type customValidator struct {
 
 func (v customValidator) Validate(i interface{}) error {
 	return v.validator.Struct(i)
+}
+
+type storageShadowWithFailedStore struct {
+	redisStorage.RedisStorage
+}
+
+func (s storageShadowWithFailedStore) BuildConfiguration(c models.Config) (models.StorageInterface, error) {
+	return s, nil
+}
+
+func (s storageShadowWithFailedStore) Store(id uuid.UUID, data []byte, duration time.Duration) error {
+	return errors.New("an error")
 }
 
 func TestDI_PostStoreSecureNoteV1WithNoValidData(t *testing.T) {
@@ -91,6 +108,48 @@ func TestDI_PostStoreSecureNoteV1WithInvalidPayload(t *testing.T) {
 	if responseModel.Message != "validation failed" ||
 		responseModel.Error[0] != "validation failed for field: SelfDestruct. "+
 			"reason: oneof. additional data: 0 1 3 6 12 24 48 72 168 720" {
+		t.Fail()
+	}
+}
+
+func TestDI_PostStoreSecureNoteV1WithValidPayloadAndStorageInterfaceFailure(t *testing.T) {
+	storage.Storage["shadowWithFailedStore"] = storageShadowWithFailedStore{}
+	c := config.BuildConfig()
+	c.StorageDriver = "shadowWithFailedStore"
+
+	di, err := BuildDI(c)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(postValidPayload))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+
+	// injecting validator
+	v := validator.New()
+	e.Validator = &customValidator{validator: v}
+
+	//build context
+	context := e.NewContext(req, rec)
+
+	err = di.PostStoreSecureNoteV1(context)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if rec.Code != http.StatusInternalServerError {
+		t.Fail()
+	}
+
+	responseModel := models.Response{}
+	err = json.Unmarshal(rec.Body.Bytes(), &responseModel)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if responseModel.Message != "internal error occurred" {
 		t.Fail()
 	}
 }
