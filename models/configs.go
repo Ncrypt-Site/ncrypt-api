2cf1d2a393500d90b2646b1b0a62c0bd1ee9d8ff
Farshad Nematdoust <farshad@nematdoust.com>
2020-05-27 20:21:06 +0200

    Add ApplicationConfig to config model This will serve the purpose of application configuration.

--- models/configs.go
+++ models/configs.go
@@ -3,10 +3,19 @@ package models
 type Config struct {
 	StorageDriver string
 	RedisConfig
+	ApplicationConfig
+}
+
+type ApplicationConfig struct {
+	ApplicationUrlConfig
 }
 
 type RedisConfig struct {
 	Addr     string
 	Password string
 	Database int
+}
+
+type ApplicationUrlConfig struct {
+	ApiBaseUrl string
 }
