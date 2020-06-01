0cf65004f4e81dd8b44ed1e04574d724b939b9a3
Farshad Nematdoust <farshad@nematdoust.com>
2020-06-01 20:07:01 +0200

    Add comments and fix lint in configs.go

--- models/configs.go
+++ models/configs.go
@@ -1,21 +1,25 @@
 package models
 
+// Config holds the application configuration data
 type Config struct {
 	StorageDriver string
 	RedisConfig
 	ApplicationConfig
 }
 
+// ApplicationConfig holds application configuration data
 type ApplicationConfig struct {
 	ApplicationUrlConfig
 }
 
+// RedisConfig holds redis connection configuration data
 type RedisConfig struct {
 	Addr     string
 	Password string
 	Database int
 }
 
+// ApplicationUrlConfig holds different URLs used inside the app
 type ApplicationUrlConfig struct {
 	ApiBaseUrl string
 	AppBaseUrl string
