82a884dd636299e17c628ab40bc2dadf89183ffd
Farshad Nematdoust <farshad@nematdoust.com>
2020-06-01 20:09:43 +0200

    Add comments and fix lint in index.go

--- handlers/index.go
+++ handlers/index.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
+//Index handles app entry point and redirect to /api/v1
 func (di *DI) Index(c echo.Context) error {
 	return c.Redirect(http.StatusMovedPermanently, "/api/v1")
 }
