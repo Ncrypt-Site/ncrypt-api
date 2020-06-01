e6d77bd59fb8cf0d844b5ba29583ed4eac44a642
Farshad Nematdoust <farshad@nematdoust.com>
2020-06-01 20:10:32 +0200

    Add comments and fix lint in response_builder.go

--- helpers/response_builder.go
+++ helpers/response_builder.go
@@ -2,6 +2,7 @@ package helpers
 
 import "ncrypt-api/models"
 
+//BuildResponse is a helper to build response in a defined format
 func BuildResponse(c int, m string, d interface{}, e []string, meta interface{}) models.Response {
 	return models.Response{
 		Code:    c,
