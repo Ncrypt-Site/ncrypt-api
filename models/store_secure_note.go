0e021266100f23b7907adf020cbf8e251cdc36c0
Farshad Nematdoust <farshad@nematdoust.com>
2020-06-01 20:19:19 +0200

    Add comments and fix lint in store_secure_note.go

--- models/store_secure_note.go
+++ models/store_secure_note.go
@@ -1,16 +1,19 @@
 package models
 
+//SecureMessageRequest
 type SecureMessageRequest struct {
 	Note                 string `json:"message" validate:"required"`
 	SelfDestruct         int    `json:"self_destruct" validate:"oneof=0 1 3 6 12 24 48 72 168 720"`
 	DestructAfterOpening bool   `json:"destruct_after_opening" validate:"omitempty"`
 }
 
+//SecureMessageResponse
 type SecureMessageResponse struct {
 	Id  string `json:"id"`
 	URL string `json:"url"`
 }
 
+//SecureMessage
 type SecureMessage struct {
 	Note                 []byte
 	DestructAfterOpening bool
