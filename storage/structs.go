9e3c90e706c2e55ffa6e5b548da083d7bcd4264d
Farshad Nematdoust <farshad@nematdoust.com>
2020-06-01 20:11:20 +0200

    Add comments and fix lint in structs.go

--- storage/structs.go
+++ storage/structs.go
@@ -2,6 +2,7 @@ package storage
 
 import redisStorage "ncrypt-api/storage/redis-storage"
 
+//Storage holds the list of all supported storage interfaces
 var Storage = map[string]interface{}{
 	"redis": &redisStorage.RedisStorage{},
 }
