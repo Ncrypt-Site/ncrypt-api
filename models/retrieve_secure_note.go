81208af2397f3082c4e0fff52b0409e26b4b1904
Farshad Nematdoust <farshad@nematdoust.com>
2020-06-01 20:12:45 +0200

    Add comments and fix lint in retrieve_secure_note.go

--- models/retrieve_secure_note.go
+++ models/retrieve_secure_note.go
@@ -2,10 +2,12 @@ package models
 
 import "github.com/google/uuid"
 
+//RetrieveNoteRequest
 type RetrieveNoteRequest struct {
 	Id uuid.UUID `json:"id" validate:"required"`
 }
 
+//RetrieveNoteResponse
 type RetrieveNoteResponse struct {
 	Note string `json:"note"`
 }
