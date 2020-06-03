96bd904b9ef6b690781fcbba3ba6fde0ebd1d2a6
Farshad Nematdoust <farshad@nematdoust.com>
2020-06-03 19:37:27 +0200

    Update message in /api/v1

--- handlers/get_api_index.go
+++ handlers/get_api_index.go
@@ -7,5 +7,5 @@ import (
 
 //GetIndexV1 handles GET /api/v1
 func (di *DI) GetIndexV1(c echo.Context) error {
-	return c.JSON(200, helpers.BuildResponse(200, "NCrypt API v: 1.0.0", nil, nil, nil))
+	return c.JSON(200, helpers.BuildResponse(200, "nCrypt API v: 1.0.0", nil, nil, nil))
 }
